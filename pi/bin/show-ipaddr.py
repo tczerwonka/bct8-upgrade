fc6bb7f195f2a1ed0102723a98fe8b2d867de7b7
T Czerwonka <tczerwonka@gmail.com>
2022-01-15 18:27:55 +0000

    not as dumb as the last one

--- pi/bin/show-ipaddr.py
+++ pi/bin/show-ipaddr.py
@@ -3,10 +3,14 @@
 
 #read test
 
+
 import sys
 import time
 import os
 import serial
+import socket
+
+
 
 #open serial port
 ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
@@ -15,13 +19,24 @@ while True:
    if data:
       print data
    if (data == "d"):
-      ser.write("192\r\n")
+      #depends on route out but whatever
+      s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+      s.connect(("8.8.8.8", 80))
+      octets = s.getsockname()[0].split('.')
+      ser.write('@\r\n')
+      ser.write(octets[0].ljust(3))
+      ser.write('\r\n')
       time.sleep(1)
-      ser.write("168\r\n")
+      ser.write(octets[1].ljust(3))
+      ser.write('\r\n')
       time.sleep(1)
-      ser.write("001\r\n")
+      ser.write(octets[2].ljust(3))
+      ser.write('\r\n')
       time.sleep(1)
-      ser.write("068\r\n")
+      ser.write(octets[3].ljust(3))
+      ser.write('\r\n')
+      time.sleep(1)
+      ser.write('@\r\n')
 
 
 
